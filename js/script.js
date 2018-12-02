54f88cd5650baad4286dcef99d8fa6c76cb1cae2
Matt Drapchaty <Drapchaty@comcast.net>
2018-12-02 00:24:22 -0700

    smooth scrolling, gradient on list items bg

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(function() {
-    console.log("hello");
+    console.log("hello world");
+
+//Smooth scrolling on link click
+    $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
 });
