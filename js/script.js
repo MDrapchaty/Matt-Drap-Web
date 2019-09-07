cf4b2a27a75ffd333272b550e9a8ce21d48721ad
Matt Drapchaty <Drapchaty@comcast.net>
2019-09-07 00:05:41 -0600

    modals fixed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $(function() {
     
 
-
 //Smooth scrolling on link click
     $(document).on('click', 'a[href^="#"]', function (event) {
       event.preventDefault();
@@ -26,40 +25,61 @@ $(function() {
 
 
 
-  /* modal */ 
+  /* modal */
 var work = document.getElementsByClassName('work_example');
+var modal = [];
+var modals = document.getElementsByClassName('modal');
 
 /* for loop to run modal code for each work example modal */
 var i;
-for (i = 0; i < work.length; i++) { 
+for (i = 0; i < 6; i++) { 
   
   // Get the modal
-  var modal = document.getElementsByClassName('myModal')[i];
-
+  modal[i] = document.getElementById('myModal' + i);
+  console.log(modal[i]);
   // Get the button that opens the modal
-  var btn = document.getElementsByClassName("work_button")[i];
-
+  var btn = document.getElementsByClassName('work_button')[i];
+  console.log(btn);
   // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[i];
-
+  var span = document.getElementsByClassName('close')[i];
+  let test = modal[i];
+  
   // When the user clicks on the button, open the modal 
   btn.onclick = function() {
-    modal.style.display = "block";
+    test.style.display = "block";
+    
   }
 
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
-    modal.style.display = "none";
+    test.style.display = "none";
   }
 
   // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
+ 
 
 }
+
+ window.onclick = function(event) {
+    if (event.target == modal[0]) {
+      modal[0].style.display = "none";
+    }
+    if (event.target == modal[1]) {
+      modal[1].style.display = "none";
+    }
+    if (event.target == modal[2]) {
+      modal[2].style.display = "none";
+    }
+    if (event.target == modal[3]) {
+      modal[3].style.display = "none";
+    }
+    if (event.target == modal[4]) {
+      modal[4].style.display = "none";
+    }
+    if (event.target == modal[5]) {
+      modal[5].style.display = "none";
+    }
+  }
 
 
     /* show navbar at top of page */
