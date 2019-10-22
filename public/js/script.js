252001c08120a42d615cdd92df5367fe83d33828
Matt Drapchaty <Drapchaty@comcast.net>
2019-10-22 16:10:41 -0600

    mobile viewport fix attempts

--- public/js/script.js
+++ public/js/script.js
@@ -100,11 +100,12 @@ window.addEventListener('resize', () => {
 });
 
 //Viewport fix for mobile safari/ios
+/*
 window.onresize = function() {
     document.body.height = window.innerHeight;
 }
 window.onresize(); // called to initially set the height.
-
+*/
 
 
 });
